14ff41e7fe60b3fe30025e29151645f94f9a1d22
NAKAAYI SHAMIRAH <shamienakaayi@gmail.com>
2025-07-16 00:26:28 -0700

    Add files via upload

---
+++ input and arithmetic.py
@@ -0,0 +1,13 @@
+#get user input for length and width
+
+length = float(input("enter length:"))
+width = float(input("enter width:"))
+#Calculate the area and perimeter
+Length = 10
+Width = 5
+area = length * width
+perimeter = 2* (length + width)
+#calculate the area and perimeter
+print(f"Area: {length * width}")
+print(f"Perimeter: {2 *(length + width)}")
+                                     
